064bd015092ec3165cc62a0c8b5d5abb3878ea68
Brian Miles <selimnairb@gmail.com>
2013-07-19 11:28:18 -0400

    - Updated version numbers for 1.01 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='rhessysworkflows',
-      version='1.0',
+      version='1.01',
       description='Libraries and command-line scripts for performing RHESSys data preparation workflows.',
       long_description=readme(),
       classifiers=[
@@ -25,7 +25,7 @@ setup(name='rhessysworkflows',
                 'rhessysworkflows.tests'
                 ],
       install_requires=[
-        'ecohydrolib==1.0'
+        'ecohydrolib==1.01'
       ],
       scripts=['bin/CreateFlowtable.py',
                'bin/CreateGRASSLocationFromDEM.py',
