da9a6450c286ce1b5150a96857c2e593d3d60f51
Brian Miles <selimnairb@gmail.com>
2015-03-18 21:21:21 -0400

    1.28 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='rhessysworkflows',
-      version='1.27',
+      version='1.28',
       description='Libraries and command-line scripts for performing RHESSys data preparation workflows.',
       long_description=readme(),
       classifiers=[
